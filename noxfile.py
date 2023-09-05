611673de33725a63bc775c49412c054f479e50c4
Snorre Ludvig Fjellvang <slf@sevanssp.com>
2023-09-05 15:42:18 +0200

    Initial commit

---
+++ noxfile.py
@@ -0,0 +1,52 @@
+import nox
+import tempfile
+import os
+
+# override default sessions
+nox.options.sessions = ["lint", "tests"]
+
+
+@nox.session
+def lint(session):
+    """Highlight syntactical and stylistic problems in the code."""
+    session.install("flake8")
+    session.run(
+        "flake8",
+        "inquire_assets_sdk/",
+        "--count",
+        "--select=E9,F63,F7,F82",
+        "--show-source",
+        "--statistics",
+    )
+    session.run(
+        "flake8",
+        "inquire_assets_sdk/",
+        "--count",
+        "--per-file-ignores=__init__.py:F401",
+        "--exit-zero",
+        "--max-complexity=10",
+        "--max-line-length=127",
+        "--statistics",
+    )
+
+
+@nox.session
+def tests(session):
+    """Run test suite."""
+    # install dependencies
+    req_path = os.path.join(tempfile.gettempdir(), 'requirements.txt')
+    session.install("poetry")
+
+    session.run(
+        "poetry",
+        "export",
+        "--with=dev",
+        "--format=requirements.txt",
+        f"--output={req_path}",
+        external=True,
+    )
+    session.install("-r", req_path)
+
+    # run tests
+    session.run("pytest", "-s", "tests")
+
