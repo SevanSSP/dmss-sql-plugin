a7766dd47d13fa594f7fed0bccead07b0f7d4c68
Snorre Ludvig Fjellvang <slf@sevanssp.com>
2023-09-15 16:22:45 +0200

    Rearranged slightly

--- plugin/dev.py
+++ plugin/dev.py
@@ -1,13 +1,13 @@
 from plugin.models import Blueprint, Attribute, ExtendedBaseModel
 
-json_file_path = 'plugin/study.json'
+json_file_path = 'blueprints/study.json'
 
 with open(json_file_path, "r") as json_file:
     json_data = json_file.read()
 
 blueprint_study = Blueprint.model_validate_json(json_data)
 
-json_file_path = 'plugin/case.json'
+json_file_path = 'blueprints/case.json'
 
 with open(json_file_path, "r") as json_file:
     json_data = json_file.read()
